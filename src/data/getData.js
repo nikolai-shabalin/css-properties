4c27b20ab086d37cc3b5dcef893a1cb153798feb
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-10-15 10:47:47 +0300

    Create data

---
+++ src/data/getData.js
@@ -0,0 +1,30 @@
+import bcd from '@mdn/browser-compat-data' assert { type: "json" };
+import fs from 'node:fs';
+
+const { css, browsers } = bcd;
+let data = [];
+
+const getSpecUrl = (properties, propertyName) => {
+    const specUrl = properties[propertyName]['__compat']['spec_url'];
+    return Array.isArray(specUrl) ? specUrl[0] : specUrl;
+}
+
+for (const categoryKey in css) {
+    const category = categoryKey;
+    const properties = css[category];
+
+    for (const propertyName in css[category]) {
+        const name = propertyName;
+        const mdnUrl = properties[propertyName]['__compat']['mdn_url'];
+        const specUrl = getSpecUrl(properties, propertyName);
+
+        data.push({
+            category,
+            name,
+            mdnUrl,
+            specUrl
+        });
+    }
+}
+
+export default data;
