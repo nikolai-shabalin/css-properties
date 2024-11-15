74c589976ff01a1ec8fc61dae0fbb9fd9e56214a
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-11-15 19:19:56 +0300

    Remove trash

--- src/data/getData.js
+++ src/data/getData.js
@@ -1,32 +1,28 @@
-import bcd from '@mdn/browser-compat-data' assert { type: "json" };
-import { log } from 'node:console';
-import fs from 'node:fs';
+import bcd from '@mdn/browser-compat-data' with {type: "json"};
 
 // TODO: добыть подствойства
-const { css, browsers } = bcd;
+const {css, browsers} = bcd;
 export let data = [];
 
 const getSpecUrl = (properties, propertyName) => {
-    const specUrl = properties[propertyName]['__compat']['spec_url'];
-    return Array.isArray(specUrl) ? specUrl[0] : specUrl;
+  const specUrl = properties[propertyName]['__compat']['spec_url'];
+  return Array.isArray(specUrl) ? specUrl[0] : specUrl;
 }
 
 for (const categoryKey in css) {
-    const category = categoryKey;
-    const properties = css[category];
+  const category = categoryKey;
+  const properties = css[category];
 
-    for (const propertyName in css[category]) {
-        const name = propertyName;
-        const mdnUrl = properties[propertyName]['__compat']['mdn_url'];
-        const specUrl = getSpecUrl(properties, propertyName);
+  for (const propertyName in css[category]) {
+    const name = propertyName;
+    const mdnUrl = properties[propertyName]['__compat']['mdn_url'];
+    const specUrl = getSpecUrl(properties, propertyName);
 
-        data.push({
-            category,
-            name,
-            mdnUrl,
-            specUrl
-        });
-    }
+    data.push({
+      category,
+      name,
+      mdnUrl,
+      specUrl
+    });
+  }
 }
-
-console.log(data.length);
