f7a1a4a5beac7619effc79e9b757a027fa75b103
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2025-06-17 17:56:57 +0300

    Add English version (#42)
    * Add English version
    
    * Fix en component

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+declare module '../components/HeadEn.astro';
+declare module '../components/HeroEn.astro';
+declare module '../components/YearlyChartEn.astro';
+declare module '../components/CssEraEn.astro';
+declare module '../components/PropertiesEn.astro';
+declare module '../components/PropertyEn.astro';
+declare module '../components/ScrollToTopEn.astro';
+declare module '../components/GithubEn.astro';
+declare module '../components/SelectYearEn.astro';
+declare module '../layouts/BaseEn.astro';
